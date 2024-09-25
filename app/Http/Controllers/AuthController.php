83d7324e45cc5213582e3e88541d61aa6a56ee71
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2024-09-25 11:01:38 -0300

    feat: adicao do controller authControler, e adição das rotas da api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+        $user->token = $token;
+
+        $user->addRole(3); // add Participante Role
+
+        $resource = new UserResource($user);
+        return $resource->response()->setStatusCode(201);
+    }
+
+    public function addRole(string $id, Request $request)
+    {
+        $data = $request->validate([
+            'role_id' => 'required|exists:roles,id',
+        ]);
+
+        $user = User::find($id);
+        if ($user) {
+            $user->addRole($data['role_id']);
+            return new UserResource($user);
+        }
+        return response(['error' => 'Usuário não encontrado.'], 404);
+    }
+
+    public function removeRole(string $id, Request $request)
+    {
+        $data = $request->validate([
+            'role_id' => 'required|exists:roles,id',
+        ]);
+
+        $user = User::find($id);
+        if ($user) {
+            $user->removeRole($data['role_id']);
+            return new UserResource($user);
+        }
+        return response(['error' => 'Usuário não encontrado.'], 404);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response(['error' => 'O e-mail informado não está cadastrado.'], 401); //Unauthorized
+        }
+
+        if ($user and Hash::check($request->password, $user->password)) {
+            $token = $user->createToken('auth-token')->plainTextToken;
+            $user->token = $token;
+
+            return new UserResource($user);
+        }
+
+        return response(['error' => 'A senha informada está incorreta.'], 401); //Unauthorized
+    }
+
+    public function validateToken(Request $request)
+    {
+        if ($token = $request->bearerToken()) {
+            $user = auth('sanctum')->user();
+            $user->token = $token;
+            return new UserResource($user);
+        }
+    }
+
+
+    public function logout()
+    {
+        /** @var User $user */
+        $user = Auth()->user();
+        $user->tokens()->delete();
+
+        return response(['message' => 'Logout realizado com sucesso.'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\IncomeController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +22,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/validate-token', [AuthController::class, 'validateToken']);
+
+    // Rotas de usuários
+    Route::get('/users', [UserController::class, 'index'])->name('user.index');
+    Route::get('/users/{user}', [UserController::class, 'show'])->name('user.show');
+    Route::post('/users', [UserController::class, 'store'])->name('user.store');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+
+    // Rotas de transações
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transaction.index');
+    Route::get('/transactions/{transaction}', [TransactionController::class, 'show'])->name('transaction.show');
+    Route::post('/transactions', [TransactionController::class, 'store'])->name('transaction.store');
+    Route::put('/transactions/{transaction}', [TransactionController::class, 'update'])->name('transaction.update');
+    Route::delete('/transactions/{transaction}', [TransactionController::class, 'destroy'])->name('transaction.destroy');
+
+    // Rotas de contas
+    Route::get('/accounts', [AccountController::class, 'index'])->name('account.index');
+    Route::get('/accounts/{account}', [AccountController::class, 'show'])->name('account.show');
+    Route::post('/accounts', [AccountController::class, 'store'])->name('account.store');
+    Route::put('/accounts/{account}', [AccountController::class, 'update'])->name('account.update');
+    Route::delete('/accounts/{account}', [AccountController::class, 'destroy'])->name('account.destroy');
+
+    // Rotas de receitas (incomes)
+    Route::get('/incomes', [IncomeController::class, 'index'])->name('income.index');
+    Route::get('/incomes/{income}', [IncomeController::class, 'show'])->name('account.show');
+    Route::post('/incomes', [IncomeController::class, 'store'])->name('account.store');
+    Route::put('/incomes/{income}', [IncomeController::class, 'update'])->name('account.update');
+    Route::delete('/incomes/{income}', [IncomeController::class, 'destroy'])->name('account.destroy');
+
+    // Rotas de despesas (expenses)
+    Route::get('/expenses', [ExpenseController::class, 'index'])->name('expense.index');
+    Route::get('/expenses/{expense}', [ExpenseController::class, 'show'])->name('expense.show');
+    Route::post('/expenses', [ExpenseController::class, 'store'])->name('expense.store');
+    Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expense.update');
+    Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expense.destroy');
+
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('accounts', AccountController::class);
+    Route::apiResource('expenses', ExpenseController::class);
+    Route::apiResource('incomes', IncomeController::class);
+    Route::apiResource('transactions', TransactionController::class);
 });
+
