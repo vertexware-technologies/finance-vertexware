1d63d0e339e6a06bdaaa0bbda97b8a43e9b74d3d
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2024-11-18 11:28:07 -0300

    correcao delete

--- resources/views/livewire/transactions/button-transaction-delete.blade.php
+++ resources/views/livewire/transactions/button-transaction-delete.blade.php
@@ -1,7 +1,10 @@
 <div>
     <!-- Botão de exclusão -->
     <button wire:click="confirmDelete({{ $transaction->id }})" class="h-4 w-4 focus:outline-none">
-        @svg('eos-delete', ['style' => 'color: red;'])
+        <svg class="w-64 h-64" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="red">
+            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path>
+        </svg>
+
     </button>
 
     <!-- Modal de confirmação -->
